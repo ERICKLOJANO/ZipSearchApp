cc4ec43a53dcae6f7136fa9b166a760920cf354b
ErickLojano <erickjlojano@gmail.com>
2021-01-11 20:37:19 -0500

    added styling, and additional values to be shown when a user inputs a zip code

--- src/ZipCode.jsx
+++ src/ZipCode.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class ZipCode extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { Zipcode: 10006, City: 'No results' };
+		this.state = {
+			Zipcode: 10006,
+			City: 'No results',
+			State: '',
+			Lat: '',
+			Long: '',
+			Population: ''
+		};
 		this.searchCities = this.searchCities.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 	}
@@ -15,7 +22,16 @@ class ZipCode extends Component {
 		axios
 			.get(`http://ctp-zip-api.herokuapp.com/zip/${this.state.Zipcode}`)
 			.then((response) => {
-				this.setState({ City: response.data[0].City });
+				this.setState({
+					City: response.data[0].City,
+					State: response.data[0].State,
+					Lat: response.data[0].Lat,
+					Long: response.data[0].Long,
+					EstimatedPopulation: response.data[0].EstimatedPopulation,
+					TotalWages: response.data[0].TotalWages
+
+				 });
+				console.log(response)
 			})
 			.catch((error) => {
 				this.setState({ City: 'No results' });
@@ -27,14 +43,23 @@ class ZipCode extends Component {
 	render() {
 		return (
 			<div>
+				<div id = "titleContainer">
+				<h1 id = "mainTitle">Zip Code Search</h1>
+				</div>
 				<form onSubmit={this.searchCities}>
+					<div id = "userInput">
 					<label>
 						Enter ZipCode:
 						<input type="text" value={this.state.Zipcode} onChange={this.handleChange}></input>
 					</label>
-					<input type="submit" value="Submit"></input>
+						<input type="submit" value="Submit"></input>
+					</div>
 				</form>
 				<p>This ZipCode is in {this.state.City}</p>
+				<p>State: {this.state.State}</p>
+				<p>Location: ({this.state.Lat},{this.state.Long})</p>
+				<p>Population(estimated): {this.state.EstimatedPopulation}</p>
+				<p>Total Wages: {this.state.TotalWages}</p>
 			</div>
 		);
 	}
